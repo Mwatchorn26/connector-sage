09284280922753dd90dc2abca3f31e54eefc7a75
Michael Watchorn <mwatchorn26@gmail.com>
2019-07-04 12:05:40 -0400

    Started a port from old API to the new API - Incomplete.
    Odoo 10 doesn't use the the old API, and needs the code to be ported to the new API.
    I've started porting the wizard's py file, nothing else (yet).

--- exportsage50/wizard/exportsage50.py
+++ exportsage50/wizard/exportsage50.py
@@ -19,18 +19,18 @@
 #
 ##############################################################################
 
-from osv import osv, fields
+from openerp import models, fields, api, exceptions
 import web
 import base64
 import tools
 from tools.translate import _
 from tools.misc import get_iso_codes
-import pooler
+#import pooler
 from datetime import datetime
 import decimal_precision as dp
 
 
-class exportsage(osv.osv):
+class exportsage(models.TransientModel):
 
     """
     Wizard 
@@ -38,48 +38,51 @@ class exportsage(osv.osv):
     _name = "exportsage"
     _description = "Create imp file  to export  in sage50"
     _inherit = "ir.wizard.screen"
-    _columns = {
-        'data': fields.binary('File', readonly=True),
-        'name': fields.char('Filename', 20, readonly=True),
-        'format': fields.char('File Format', 10),
-        'state': fields.selection([('choose', 'choose'), # choose date
-                                   ('get', 'get')]),
-        'invoice_ids': fields.many2many('account.invoice', 'sale_order_invoice_export_rel', 'order_id', 'invoice_id',
+    data = fields.binary('File', readonly=True)
+    name = fields.char('Filename', 20, readonly=True)
+    format = fields.char('File Format', 10)
+    state = fields.selection([('choose', 'choose'), # choose date
+                                   ('get', 'get')], lambda *a: 'choose')
+    invoice_ids = fields.many2many('account.invoice', 'sale_order_invoice_export_rel', 'order_id', 'invoice_id',
                                         'Invoices', required=True,
                                         help="This is the list of invoices that have been generated for this sales order. The same sales order may have been invoiced in several times (by line for example)."),
-    }
-
-    _defaults = {
-        'state': lambda *a: 'choose',
-    }
-    def act_cancel(self, cr, uid, ids, context=None):
+    
+    # MOVED TO state FIELD DEFINITION
+    #_defaults = {
+    #    'state': lambda *a: 'choose',
+    #}
+    def act_cancel(self):
         #self.unlink(cr, uid, ids, context)
         return {'type':'ir.actions.act_window_close' }
 
 
-    def act_destroy(self, *args):
+    #def act_destroy(self, *args):
+    def act_destroy(self):
         return {'type':'ir.actions.act_window_close' }   
 
  
-    def create_report(self, cr, uid, ids, context=None):
+    def create_report(self):
         if context == None:
             context = {}
-        this = self.browse(cr, uid, ids)[0]
+        #this = self.browse(cr, uid, ids)[0]
         data = self.read(cr, uid, ids, [], context=context)[0]
 
         if not data['invoice_ids']:
-            raise osv.except_osv(_('Error'), _('You have to select at least 1 Invoice. And try again'))
-
+            #raise osv.except_osv(_('Error'), _('You have to select at least 1 Invoice. And try again'))
+            raise exceptions.UserError(_('You have to select at least 1 Invoice, and try again.') 
+            
         output = '<Version>''\n' + '"12001"' + ',' + '"1"''\n' + '</Version>\n\n'
-        #Faire le traitement des autres lignes dans les lignes de factures
-        pool = pooler.get_pool(cr.dbname)
-        line_obj = pool.get('account.invoice')
-
-        for line in line_obj.browse(cr, uid, data['invoice_ids'], context):
-            # tag de debut pour les lignes de factures
+        #Process the other lines in the invoice lines
+        #v7: pool = pooler.get_pool(cr.dbname)
+        #v7: line_obj = pool.get('account.invoice')
+        invoices = env['account.invoice']
+                                       
+        #v7: for line in line_obj.browse(cr, uid, data['invoice_ids'], context):
+        for line in invoices.lines 
+            #Start tag for invoice lines
             output += '<SalInvoice>''\n'
-            #informations sur le client
-            costumer_name = line.partner_id.name
+            #Information about the client
+            customer_name = line.partner_id.name
             oneTimefield = ""
             contact_name = line.partner_id.address[0].name or ""
             street1 = line.partner_id.address[0].street or ""
@@ -92,15 +95,15 @@ class exportsage(osv.osv):
             mobile = line.partner_id.address[0].mobile or ""
             fax = line.partner_id.address[0].fax or ""
             email = line.partner_id.address[0].email or ""
-            # ligne de client
-            fields = [costumer_name, oneTimefield, contact_name, street1, street2,
+            # Client Line:
+            fields = [customer_name, oneTimefield, contact_name, street1, street2,
                       city, province_state, zip_code, country, phone1, mobile, fax, email
                       ]
-            costumer = ','.join(['"%s"' % field for field in fields])
-            #print costumer
+            customer = ','.join(['"%s"' % field for field in fields])
+            #print customer
             #exit(0)
-            output += costumer.encode('UTF-8') + '\n'
-            #informations sur la facture
+            output += customer.encode('UTF-8') + '\n'
+            #Information about the Invoice
             no_of_details = str(len(line.invoice_line))
             order_no = ""
             # Invoice number (Max 20 chars)
@@ -110,9 +113,9 @@ class exportsage(osv.osv):
                 entry_date = datetime.strptime(line.date_invoice, "%Y-%m-%d").strftime('%m-%d-%Y')  # date format : mm-dd-yyyy
             else:
                 entry_date = ""
-            # Informations sur le type de paiement (between 0 and 3)
+            # Payment Type Information (between 0 and 3)
             # 0 = pay later , 1 = cash , 2 = cheque , 3 = credit card
-            # Selectionner le dernier paiement
+            # Select the last payment
             list_id = []
             # Paid by source (20 Chars) : Blank- pay later and cash Cheque number or credit card
             paid_by_source = ""
@@ -120,14 +123,17 @@ class exportsage(osv.osv):
                 for oneId in line.payment_ids:
                     list_id.append(oneId.id)
                 lastId = max(list_id)
-                # acceder à partir du dernier paiement à l'objet account_move_line
-                account_move_line_obj = self.pool.get('account.move.line')
-                account_move_line = account_move_line_obj.browse(cr, uid, lastId, context=context)
-                paiement_type = account_move_line.journal_id.type
-                if paiement_type == 'cash':
+
+                #Access from the last payment to the account_move_line object
+                #v7: account_move_line_obj = self.pool.get('account.move.line')
+                account_move_lines = env['account.move.line']
+                #account_move_line = account_move_line_obj.browse(cr, uid, lastId, context=context)
+                account_move_line = account_move_lines.filter('Id'=lastId)
+                payment_type = account_move_line.journal_id.type
+                if payment_type == 'cash':
                     paid_by_type = str(1)
                     paid_by_source = account_move_line.ref
-                elif paiement_type == 'bank':
+                elif payment_type == 'bank':
                     paid_by_type = str(2)
                 else:
                     paid_by_type = str(0) # default value 0 = pay later
@@ -148,7 +154,8 @@ class exportsage(osv.osv):
             product_ids = account_invoice_line_obj.search(cr, uid, [('invoice_id','=',line.id)])
 
             if product_ids:
-                for product in account_invoice_line_obj.browse(cr, uid, product_ids):
+                #for product in account_invoice_line_obj.browse(cr, uid, product_ids):
+                for product in product_ids:
                     item_number = str(product.name)
                     quantity = str(product.quantity)
                     price = str(product.price_unit)
@@ -159,7 +166,7 @@ class exportsage(osv.osv):
                     #one_product_invoice = '"' + item_number + '"' + ',"' + quantity + '"' + ',"' + price + '"' + ',"' + amount + '"'
                     one_product_invoice = one_product_invoice.encode('UTF-8')
                     tax_product_line = ""
-                    # tax information pour chaque produit
+                    # tax information for each product
                     if product.invoice_line_tax_id:
                         for one_taxe in product.invoice_line_tax_id:
                             tax_name = one_taxe.description # or one_taxe.description or one_taxe.name
@@ -177,19 +184,18 @@ class exportsage(osv.osv):
 
                         #tax_product_line = tax_product_line[:-1]
                         tax_product_line = tax_product_line.encode('UTF-8')
-                    product_line_invoice_with_taxe += one_product_invoice + tax_product_line + '\n'
+                    product_line_invoice_with_tax += one_product_invoice + tax_product_line + '\n'
                 #print product_line_invoice_with_taxe , exit(0)
 
-                output += product_line_invoice_with_taxe
-            # tag de fin  pour les lignes de factures
-            #output += '</SalInvoice>\n'
+                output += product_line_invoice_with_tax
+            #Ending tag for Invoice lines
             output += '</SalInvoice>\n\n\n'
         #output += '\n' + this.start_date + ',' + this.end_date
-        this.format = 'imp'
+        self.format = 'imp'
         filename = 'export_to_sage50'
-        this.name = "%s.%s" % (filename, this.format)
+        self.name = "%s.%s" % (filename, self.format)
         out = base64.encodestring(output)
-        self.write(cr, uid, ids, {'state':'get', 'data':out, 'name':this.name, 'format' : this.format}, context=context)
+        self.write(cr, uid, ids, {'state':'get', 'data':out, 'name':self.name, 'format' : self.format}, context=context)
         
 exportsage()
 
